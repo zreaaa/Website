268879f77c1285841ee47e2fab14afe3bb660bbb
zreaaa <jakejasmin63@gmail.com>
2025-02-11 23:45:52 +0800

    Add files via upload

---
+++ ahihi.js
@@ -0,0 +1,16 @@
+let retryCount = 0;
+const maxRetries = Math.floor(Math.random() * 4) + 4;
+
+function retryConnection() {
+    retryCount++;
+    
+    if (retryCount >= maxRetries) {
+        window.location.href = "surprise.html";
+    } else {
+
+        document.querySelector("h1").innerText = "Still No Connection...";
+        setTimeout(() => {
+            document.querySelector("h1").innerText = "No Internet Connection";
+        }, 1000);
+    }
+}
